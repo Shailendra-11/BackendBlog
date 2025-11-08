ab815275799fbbc95c83c8e549c9800bd00526cd
Shailendra Rai <shailendrarai153@gmail.com>
2025-11-08 15:30:29 +0530

    cha

--- app.js
+++ app.js
@@ -2,26 +2,6 @@ import express from 'express'
 import path from 'path'
 const app = express()
 
-app.get('/', (req, res) => {
-  const abstPath = path.resolve("page/Home.html")
-  res.sendFile(abstPath)
-})
-
-app.get('/login', (req, res) => {
-  const abstPath = path.resolve("page/login.html")
-  res.sendFile(abstPath)
-})
-
-app.get('/about', (req, res) => {
-  const abstPath = path.resolve("page/about.html")
-  res.sendFile(abstPath)
-})
-
-
-app.use('/' , (req, res) => {
-  const abstPath = path.resolve("page/404.html")
-  res.sendFile(abstPath)
-})
 
 
 app.listen(700 ,()=>{
